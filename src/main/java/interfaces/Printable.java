d428f6b002611f3ffa2898a683aaa82763bdff9b
Zachary Stuck <zstuck96@gmail.com>
2023-11-26 11:29:26 -0500

    Add Class Documentation to Printable

--- src/main/java/interfaces/Printable.java
+++ src/main/java/interfaces/Printable.java
@@ -1,5 +1,9 @@
 package interfaces;
 
+/**
+ * Classes implementing this interface will support the printing of their
+ * internal information in a way that is easily understood and well-structured.
+ */
 public interface Printable {
     /**
      * Prints relevant information in a way that is easily understandable in a
